c8e4906b0c5dbba3cdf4a37b4b449601cc26daca
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2022-10-20 13:10:06 -0500

    calendar works now

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import '../styles/home.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Header from '../components/Header'
+import { ReactEmbeddedGoogleCalendar } from 'react-embedded-google-calendar'
+import NavBar from '../components/NavBar.js'
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 // import CustomButton from '../../components/custom-button/custom-buttom.component'
@@ -14,47 +21,32 @@ const HomePage = () => {
     // const scrollOffset = -1*window.innerHeight*.1
 
     return(
-     <div class="site-wrapper">
-
-      <div class="site-wrapper-inner">
-
-        <div class="container">
-        {/* <Header/> */}
+      <div>
         
-          {/* <div class="masthead clearfix">
-            <div class="container inner">
-              <h3 class="masthead-brand">Cover</h3>
-              <nav>
-                <ul class="nav masthead-nav">
-                  <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Find a Run</a></li>
-                </ul>
-              </nav>
-            </div>
-          </div> */}
+      
+        <div class="container">
+          {/* <div>
+         <Navbar bg="light" expand="lg"></Navbar>
+         </div> */}
 
           <div class="inner cover">
             <h1 class="cover-heading">Welcome to the Vandy Run Club</h1>
             <p class="lead">Check the calendar below for upcoming events!</p>
             <p class="lead">
-                <div>
-                <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FChicago&src=dnVydW5jbHViYXBwbGljYXRpb25AZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=MjY0MGJlOWJjZDM5YmRjYjgxNDZhMTUwOGEyZDFmNGY3MGQxYzgxMzQ1ZDBkZDlhOTkzOWRmMmRjYmZmNTg2Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%237986CB&color=%230B8043" 
-                /*style={{border:solid 1px #777,
-                 width:"800",
-                  height:"600",
-                   frameborder:"0",
-                    scrolling:"no"}}*/></iframe>
+            </p>
+                <div class="calendar">
+                <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FChicago&src=dnVydW5jbHViYXBwbGljYXRpb25AZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=MjY0MGJlOWJjZDM5YmRjYjgxNDZhMTUwOGEyZDFmNGY3MGQxYzgxMzQ1ZDBkZDlhOTkzOWRmMmRjYmZmNTg2Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%237986CB&color=%230B8043" height= '600px' width= '600px'>
+                  </iframe> 
                 </div>
               {/* <a href="#" class="btn btn-lg btn-default">Learn more</a> */}
-            </p>
+          
           </div>
 
         </div>
 
       </div>
 
-    </div>
+    //</div>
     )
 }
 
