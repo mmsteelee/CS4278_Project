37483a4d9557199f6172790f8e08fbd188e48e34
Anneliese Breidsprecher <anneliesebreidsprecher@Annelieses-MacBook-Pro.local>
2022-10-25 11:48:05 -0500

    branch changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,29 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-window.React2 = require('react');
-console.log(window.React1 === window.React2);
 
 const Navbar = () => {
     return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Vandy Run Club</a>
+          <a class="navbar-brand" href="#">Navbar</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Links</a>
-                {/* <Link to="/Links">Links</Link> */}
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Meet the Team</a>
-                {/* <Link to="/MeetTheTeam">Meet the Team</Link> */}
+                <a class="nav-link" href="#">Features</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Find Your Run</a>
-                {/* <Link to="/FindYourRun">Find Your Run</Link> */}
+                <a class="nav-link" href="#">Pricing</a>
               </li>
             </ul>
           </div>
