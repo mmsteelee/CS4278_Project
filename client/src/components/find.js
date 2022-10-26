421f29835f3ef49bdfc824cd9ceee965af4f18f2
Anneliese Breidsprecher <anneliesebreidsprecher@Annelieses-MacBook-Pro.local>
2022-10-26 15:29:52 -0500

    all pages up

---
+++ client/src/components/find.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// //import { Link } from "react-router-dom";
+import NavBar from './NavBar.js'
+
+const FindARun = () => {
+    return (
+      <div>
+        <NavBar/>
+    
+      <div className='container'>
+        
+        <h1>Find a run </h1>
+      </div>
+      </div>
+    );
+  };
+    
+  export default FindARun;
