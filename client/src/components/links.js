0303503eed520e201d68ad6c9ecf2a9d443e6ac0
Anneliese Breidsprecher <anneliesebreidsprecher@Annelieses-MacBook-Pro.local>
2022-10-26 14:31:09 -0500

    routes working

---
+++ client/src/components/links.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// //import { Link } from "react-router-dom";
+
+const Links = () => {
+    return (
+      <div className='container'>
+        
+        <h1>Links</h1>
+      </div>
+    );
+  };
+    
+  export default Links;
