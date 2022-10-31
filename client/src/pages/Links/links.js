e99821ffd73bcad26d8906bafda2fa40af401832
Anneliese Breidsprecher <anneliesebreidsprecher@Annelieses-MacBook-Pro.local>
2022-10-31 17:14:45 -0500

    links don't look horrible anymore

--- client/src/pages/Links/links.js
+++ client/src/pages/Links/links.js
@@ -2,6 +2,8 @@ import React from 'react'
 // //import { Link } from "react-router-dom";
 import NavBar from '../../components/NavBar-component/NavBar.js'
 import Header from '../../components/header-component/header.js'
+import Logout from '../../components/logout-component/logout.js'
+
 import './links.css'
 
 const Links = () => {
@@ -12,13 +14,13 @@ const Links = () => {
           <NavBar/>
           <div className='links'>
           <table>
-            <thead><tr><th>Click on these links for our other websites!</th></tr></thead>
+            <thead> Click on these links to further connect with us!</thead>
             <tbody>
               <tr>
-                <td><a href="https://groupme.com/join_group/88408537/MKZan3OD"> GroupMe </a></td>
-                <td><a href="https://docs.google.com/spreadsheets/d/1EDIrVz3ZESnqif_Kfs86KTd_whF9-cg_210EIRAJ2dU/edit"> Practice Signups </a></td>
-                <td><a href="https://www.instagram.com/vandyrunclub/?hl=en"> Instagram Page </a></td>
-                <td><a href="https://anchorlink.vanderbilt.edu/organization/runningclub"> Join the AnchorLink </a></td>
+                <a href="https://groupme.com/join_group/88408537/MKZan3OD"> GroupMe </a>
+                <a href="https://docs.google.com/spreadsheets/d/1EDIrVz3ZESnqif_Kfs86KTd_whF9-cg_210EIRAJ2dU/edit"> Practice Signups </a>
+                <a href="https://www.instagram.com/vandyrunclub/?hl=en"> Instagram Page </a>
+                <a href="https://anchorlink.vanderbilt.edu/organization/runningclub"> Join the AnchorLink </a>
               </tr>
             </tbody>
           </table>
