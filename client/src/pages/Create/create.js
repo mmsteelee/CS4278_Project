9c2a4fe623e174941ec839d240eb598b2f2556e7
cheyenne-n <deibertcn@gmail.com>
2022-11-07 15:44:32 -0600

    modified find your run

---
+++ client/src/pages/Create/create.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import NavBar from '../../components/NavBar-component/NavBar'
+
+import MapComponent from "../../components/Map/MapComponent";
+import "ol/ol.css";
+import "./create.css";
+import Header from '../../components/header-component/header'
+
+const CreateARun = () => {
+  
+return (
+  
+  
+  <div className= "main-wrapper">
+     
+
+    <div className = "map">
+      <MapComponent />
+    </div>   
+  </div>
+  
+  );
+}
+  export default CreateARun;
