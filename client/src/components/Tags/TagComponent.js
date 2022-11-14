d7e69e21764f810dc52a76827bee8db0603eaf0a
cheyenne-n <deibertcn@gmail.com>
2022-11-14 15:52:21 -0600

    replaced global array with useState

--- client/src/components/Tags/TagComponent.js
+++ client/src/components/Tags/TagComponent.js
@@ -3,8 +3,12 @@ import { useState } from "react";
 import './TagComponent.css';
 
 let pressedArray = new Array(0,0,0,0,0,0);
-let selectedTagsArray = [];
+//let selectedTagsArray = [];
+//array of selected tags
+
 const Tags = () => {
+
+    //set button colors and selected colors
     const buttonBackgroundColor = "black";
     const clickedButtonColor = "DarkGoldenRod";
     const [ShadyButtonColor, setShadyButtonColor] = useState(buttonBackgroundColor);
@@ -14,18 +18,26 @@ const Tags = () => {
     const [MediumButtonColor,setMediumButtonColor ] = useState(buttonBackgroundColor);
     const [LongButtonColor,setLongButtonColor ] = useState(buttonBackgroundColor);
   
-
+    //array of selected tags
+    const[selectedArray,setSelectedArray] = useState([]);
+   
+    ;
     const selectShady = () => {
  
         if (pressedArray[0] === 0){
         setShadyButtonColor(clickedButtonColor);
+        setSelectedArray( arr => [...arr,"shady"]);
         pressedArray[0] = 1;
-        console.log(selectedTagsArray.push("shady")); 
+        //console.log(selectedArray.length);
+        console.info(selectedArray);
+        //console.log(this.useState.selectedArray);
+        
+        //console.log(selectedTagsArray.push("shady")); 
     }
-        console.info(pressedArray);
+        //console.info(pressedArray);
       
       console.log("shady tag pressed");
-      console.info(selectedTagsArray);
+      //console.info(selectedTagsArray);
   
     }
 
@@ -33,12 +45,14 @@ const Tags = () => {
         console.log("hills tag pressed");
         if (pressedArray[1] === 0){
         setHillsButtonColor(clickedButtonColor);
-        console.log(selectedTagsArray.push("hills"));
+        setSelectedArray( arr => [...arr,"hills"]);
+        //console.log(selectedTagsArray.push("hills"));
         pressedArray[1]= 1; 
+        console.info(selectedArray);
         }
-        console.info(pressedArray);
+        //console.info(pressedArray);
       
-        console.info(selectedTagsArray);
+        //console.info(selectedTagsArray);
     
       }
 
@@ -46,44 +60,52 @@ const Tags = () => {
         console.log("pedestrian tag pressed");
         if (pressedArray[2] === 0){
         setPedButtonColor(clickedButtonColor);
-        console.log(selectedTagsArray.push("pedestrian")); 
+        setSelectedArray( arr => [...arr,"pedestrian"]);
+        //console.log(selectedTagsArray.push("pedestrian")); 
         pressedArray[2] = 1;
+        console.info(selectedArray);
         }
-       console.info(pressedArray);
-        console.info(selectedTagsArray);
+       //console.info(pressedArray);
+        //console.info(selectedTagsArray);
     
       }
 
       const selectShort = () => {
         if (pressedArray[3] === 0){
         setShortButtonColor(clickedButtonColor);
-        selectedTagsArray.push("short"); 
+        setSelectedArray( arr => [...arr,"short"]);
+        //selectedTagsArray.push("short"); 
         pressedArray[3] = 1;
+        console.info(selectedArray);
         }
-        console.log("short tag pressed");
-        console.info(selectedTagsArray);
+        //console.log("short tag pressed");
+        //console.info(selectedTagsArray);
     
       }
 
       const selectMedium= () => {
         if (pressedArray[4] === 0){
         setMediumButtonColor(clickedButtonColor);
-        selectedTagsArray.push("medium"); 
+        setSelectedArray( arr => [...arr,"medium"]);
+       // selectedTagsArray.push("medium"); 
         pressedArray[4] = 1;
+        console.info(selectedArray);
         }
-        console.log("medium tag pressed");
-        console.info(selectedTagsArray);
+        //console.log("medium tag pressed");
+        //console.info(selectedTagsArray);
     
       }
 
       const selectLong= () => {
         if (pressedArray[5] === 0){
         setLongButtonColor(clickedButtonColor);
-        selectedTagsArray.push("long");
+        setSelectedArray( arr => [...arr,"long"]);
+       // selectedTagsArray.push("long");
         pressedArray[5] = 1;
+        console.info(selectedArray);
         } 
-        console.log("long tag pressed");
-        console.info(selectedTagsArray);
+        //console.log("long tag pressed");
+        //console.info(selectedTagsArray);
     
       }
 
@@ -96,22 +118,27 @@ const Tags = () => {
         setShortButtonColor(buttonBackgroundColor);
         setMediumButtonColor(buttonBackgroundColor);
         setLongButtonColor(buttonBackgroundColor);
+        setSelectedArray([]);
+        console.info(selectedArray);
         for (var i = 0; i <pressedArray.length; ++i){
             pressedArray[i] = 0;
         }
-        console.info(selectedTagsArray);
-        selectedTagsArray.length = 0; 
+        
+        //selectedTagsArray.length = 0; 
         console.log("clear tag pressed");
-        console.info(pressedArray);
-        console.info(selectedTagsArray);
+        //console.info(pressedArray);
+        //console.info(selectedTagsArray);
     
       }
 
     return(
         <div className= "tag-wrapper">
-           
+            {/* <div>{selectedArray.map( e =>
+          <div>{ e }</div>
+        )}
+        </div> */}
          
-            <button id="shady" onClick={selectShady}style={{ background: ShadyButtonColor }}>
+            <button id="shady" onClick={selectShady}style={{ background: ShadyButtonColor }}value="Update">
                 Shady
             </button>
             <button id="hills" onClick={selectHills}style={{ background: HillsButtonColor }}>
