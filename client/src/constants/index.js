9ae35b77e24f3d860fb2dd776554a92aef932fc0
mmsteelee <matthew.r.steele@vanderbilt.edu>
2022-11-16 17:51:36 -0600

    Turn client production to true

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,5 +1,5 @@
 // api url (where your server is hosted at)
-const production = false;
+const production = true;
 
 const BACKEND_URL = production ? process.env.REACT_APP_BACKEND_URL : 'http://localhost:8080'
 
