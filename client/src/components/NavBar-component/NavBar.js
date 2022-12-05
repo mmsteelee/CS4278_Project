7199721a360cc9a587fc0cd8265c3ede0ca92dc6
mmsteelee <matthew.r.steele@vanderbilt.edu>
2022-12-04 18:05:30 -0600

    Admin Grant and Revoke Popup

--- client/src/components/NavBar-component/NavBar.js
+++ client/src/components/NavBar-component/NavBar.js
@@ -1,21 +1,25 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 // import {Nav, Navbar, NavLink } from "react-bootstrap";
 import { Link } from "react-router-dom";
 // import'../styles/index.css';
 import '../NavBar-component/navbar.css';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import AdminPopup from "./AdminPopup";
+import { UserContext } from "../../App";
 
 
 const NavBar = ({handleClick}) => {
+  const {user} = useContext(UserContext)
 
   const [showLinks, setShowLinks] = useState(false);
+  const [openPopup, setOpenPopup] = useState(false)
+
   return (
     <div className = "navbar">
       <div className="left-side">
         <a href="/">VANDERBILT RUN CLUB</a> 
       </div>
-
       <div className="right-side">
         <div className = "text" id={showLinks ? "hidden" : ""}>
           <a href="/calendar">SCHEDULE</a>
@@ -23,12 +27,21 @@ const NavBar = ({handleClick}) => {
           <a href="/findyourrun">FIND YOUR RUN</a>
           <a href="/links">LINKS</a>
           <Button onClick={handleClick}>Logout</Button>
+          {user.role === 'admin' && 
+            <Button onClick={() => setOpenPopup(true)}>Admin</Button>
+          }
         </div>
         <div className = "dropdown-button">
           <Button onClick={()=> setShowLinks(!showLinks)}>Menu<KeyboardArrowDown /></Button>
         </div>
-     
       </div>
+      {user.role === 'admin' &&
+      <AdminPopup 
+        message={'Admin Info'}
+        open={openPopup}
+        handleClose={() => {setOpenPopup(false)}}
+      />
+      }
     </div>
   )
 }
--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,20 +1,28 @@
 const Account = require('../models/Account')
 
+const get = async (req, res) => {
+    Account.find({role: 'admin'})
+        .catch(err => console.log(err))
+        .then(result => res.status(200).send(result))
+}
+
 const grant = async (req, res) => {
     const email = req.body.email
-    Account.findOneAndUpdate({email}, {role: 'admin'})
+    Account.findOneAndUpdate({email: email}, {role: 'admin'})
         .catch(err => console.log(err))
         .then(res.status(200).send('Successfully granted admin status'))
 }
 
 const revoke = async (req, res) => {
     const email = req.body.email
-    Account.findOneAndUpdate({email}, {role: 'user'})
+    console.log(email)
+    Account.findOneAndUpdate({email: email}, {role: 'user'})
         .catch(err => console.log(err))
         .then(res.status(200).send('Successfully granted admin status'))
 }
 
 module.exports = {
     grant,
-    revoke
+    revoke,
+    get
 }
